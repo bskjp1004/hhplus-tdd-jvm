575e234ead2a24a195bc5f6cfa9a3c91804aa689
bskjp <bskjp1004@gmail.com>
2025-07-11 02:43:23 +0900

    최대 잔고 정책 추가

--- src/main/java/io/hhplus/tdd/exception/ExceptionCode.java
+++ src/main/java/io/hhplus/tdd/exception/ExceptionCode.java
@@ -2,7 +2,8 @@ package io.hhplus.tdd.exception;
 
 public enum ExceptionCode {
     INVALID_AMOUNT("E001", "요청 금액은 1 이상이어야 합니다."),
-    INSUFFICIENT_BALANCE("E002", "포인트가 부족합니다.");
+    EXCEED_MAX_BALANCE("E002", "최대 잔고 2,000,000을 초과할 수 없습니다."),
+    INSUFFICIENT_BALANCE("E003", "포인트가 부족합니다.");
 
     private final String code;
     private final String message;
--- src/main/java/io/hhplus/tdd/point/validator/UserPointValidator.java
+++ src/main/java/io/hhplus/tdd/point/validator/UserPointValidator.java
@@ -1,11 +1,17 @@
 package io.hhplus.tdd.point.validator;
 
 public class UserPointValidator {
-    public static boolean canAdd(long requestAmount){
-        return requestAmount > 0;
+    public static final long MAX_BALANCE = 2_000_000L;
+
+    public static boolean isValidRequestAmount(long requestAmount){
+        return requestAmount > 0 && requestAmount <= MAX_BALANCE;
+    }
+
+    public static boolean canAdd(long currentAmount, long requestAmount){
+        return requestAmount > 0 && currentAmount + requestAmount <= MAX_BALANCE;
     }
 
     public static boolean canUse(long requestAmount){
-        return requestAmount > 0;
+        return requestAmount > 0 && requestAmount <= MAX_BALANCE;
     }
 }
